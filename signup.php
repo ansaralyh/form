ac3f22e39993da2bbca150a5f78d1bac7a2286b3
ansaralyh <ansaralyh@gmail.com>
2023-05-01 13:59:57 +0500

    code updated

--- signup.php
+++ signup.php
@@ -1,3 +1,9 @@
+
+<?php
+include '/partials/db_connection.php';
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -9,24 +15,31 @@
 </head>
 
 <body>
-    <?php require 'partials/partials.php' ?>
+    <?php require 'partials/navbar.php' ?>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Success!</strong> Your account has been created successfully.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
     <h1 class="text-center">Welcome to sign up...!</h1>
-    <form>
+    <div class="container" class="col-md-6 mx-auto" style="max-width: 500px;" >
+    <form action="/form/sgnup.php" method="POST" >
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <label for="exampleInputEmail1" class="form-label">User name</label>
             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        
         </div>
-        <div class="mb-3">
+        <div class="mb-3" >
             <label for="exampleInputPassword1" class="form-label">Password</label>
             <input type="password" class="form-control" id="exampleInputPassword1">
         </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">confirm password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+       
+        <button type="submit" class="btn btn-primary">Sign up</button>
     </form>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
