41c30b3ba9655ab40464ee8d6e2431955090f68c
ansaralyh <ansaralyh@gmail.com>
2023-05-01 14:52:54 +0500

    code update

--- partials/db_connection.php
+++ partials/db_connection.php
@@ -11,7 +11,7 @@ if(!$conn)
     die("error" . mysqli_connect_error());
 }
 else{
-echo 'success';
+echo "Success";
 }
 
 ?>
--- signup.php
+++ signup.php
@@ -1,6 +1,21 @@
-
 <?php
-include '/partials/db_connection.php';
+   $showAlert = false;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   
+    include 'partials/db_connection.php';
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $cpassword = $_POST["cpassword"];
+    $exists = false;
+    if (($password == $cpassword) && $exists == false) {
+        $sql = "INSERT INTO `user` (`username`, `password`, `dt`) VALUES ( '$username', '$password', current_timestamp())";
+        $result = mysqli_query($conn, $sql);
+     
+        if ($result) {
+            $showAlert = true;
+        }
+    }
+}
 
 ?>
 
@@ -16,29 +31,34 @@ include '/partials/db_connection.php';
 
 <body>
     <?php require 'partials/navbar.php' ?>
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-  <strong>Success!</strong> Your account has been created successfully.
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
+
+    <?php
+    if ($showAlert) {
+        echo ' <div class="alert alert-success alert-dismissible fade show" role="alert">
+      <strong>Success!</strong> Your account has been created successfully.
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
+    }
+    ?>
     <h1 class="text-center">Welcome to sign up...!</h1>
-    <div class="container" class="col-md-6 mx-auto" style="max-width: 500px;" >
-    <form action="/form/sgnup.php" method="POST" >
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">User name</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        
-        </div>
-        <div class="mb-3" >
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">confirm password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-       
-        <button type="submit" class="btn btn-primary">Sign up</button>
-    </form>
+    <div class="container" class="col-md-6 mx-auto" style="max-width: 500px;">
+        <form action="/form/sgnup.php" method="POST">
+            <div class="mb-3">
+                <label for="username" class="form-label">User name</label>
+                <input type="text" class="form-control" id="username" aria-describedby="emailHelp">
+
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password">
+            </div>
+            <div class="mb-3">
+                <label for="cpassword" class="form-label">confirm password</label>
+                <input type="password" class="form-control" id="cpassword">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Sign up</button>
+        </form>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
